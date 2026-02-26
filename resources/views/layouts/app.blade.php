84d700cb2066a35ed4927b8046bf1616b39d7360
ayazfazlani <fazlani362@gmail.com>
2026-02-26 22:10:16 +0500

    shumburo-stock-first-commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <!-- ... existing head content ... -->
 </head>
+
 <body>
     <div class="min-h-screen bg-gray-100">
         <nav class="bg-white border-b border-gray-100">
@@ -44,11 +46,15 @@
                                     <!-- Profile/Settings Dropdown -->
                                     <x-dropdown align="right" width="48">
                                         <x-slot name="trigger">
-                                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
+                                            <button
+                                                class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
                                                 <div>{{ Auth::user()->name }}</div>
                                                 <div class="ml-1">
-                                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clip-rule="evenodd" />
                                                     </svg>
                                                 </div>
                                             </button>
@@ -72,4 +78,5 @@
         </main>
     </div>
 </body>
-</html> 
+
+</html>
